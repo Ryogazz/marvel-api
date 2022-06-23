4b528c53fbb09f2d60751d2cde7ea9fc9e16769c
ryogazz <josuex.x.x@hotmail.com>
2022-06-23 18:23:58 -0300

    feat: Pagina characters
    Implementando CSS com styled-components, na pagina de characters.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Characters from './pages/Characters';
+import GlobalStyles from './styles/GlobalStyles';
 
 const App: React.FC = () => {
-  return <Characters />;
+  return (
+    <>
+      <Characters />
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,22 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../Services/api';
 
+import { Container } from './styles';
+
+interface ResponseData {
+  id: string;
+  name: string;
+  description: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+}
+
 const Characters: React.FC = () => {
+  const [characters, setCharacters] = useState<ResponseData[]>([]);
+
   useEffect(() => {
     api
       .get('/characters')
-      .then((Response) => console.log(Response.data.data.results))
+      .then((Response) => {
+        setCharacters(Response.data.data.results);
+      })
       .catch((err) => console.log(err));
   }, []);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`${baseUrl}ts=${time}&apikey=${publicKey}&hash=${hash}`)
-  //     .then((Response) => console.log(Response.data.data.results))
-  //     .catch((err) => console.log(err));
-  // }, []);
-
-  return <div>Characters</div>;
+  return (
+    <Container>
+      <h1>Characters</h1>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.id}>
+            <img
+              src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+              alt={character.name}
+            />
+            <strong>{character.name}</strong>
+            <p>{character.description}</p>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
 };
 
 export default Characters;
